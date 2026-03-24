b541ce5f9f89613f0f289e62ad0a713bfe304c77
ELEMES1903 <lshiota@gmail.com>
2026-03-24 18:15:35 +0900

    feat: Add `index.njk` and `note.njk` layouts for page rendering, and introduce an `iconify` filter for inline icons.

--- .eleventy.js
+++ .eleventy.js
@@ -368,6 +368,14 @@ module.exports = function(eleventyConfig) {
     return `<i data-lucide="${name}" class="inline-icon"></i>`;
   });
 
+  eleventyConfig.addFilter("iconify", function(str) {
+    if (!str) return str;
+    return str.replace(/::([^:\s]+)::/g, (match, iconName) => {
+      return `<i data-lucide="${iconName}" class="inline-icon"></i>`;
+    });
+  });
+
+
 
 
 
